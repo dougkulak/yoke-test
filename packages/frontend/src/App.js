159d3d717024bb832e819be255d15370681bbc99
Doug Kulak <dougkulak@gmail.com>
2022-03-24 21:08:09 -0700

    theme: add web font and extend chakra theme

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,22 +1,33 @@
 import React from 'react';
-import {ChakraProvider, Container, theme} from '@chakra-ui/react';
+import {ChakraProvider, theme, extendTheme} from '@chakra-ui/react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import NotFound from './pages/NotFound';
+import Default from './layouts/Default';
+import Products from './pages/Products';
+
+const appTheme = extendTheme({
+  fonts: {
+    body: 'Montserrat, system-ui, sans-serif',
+    heading: 'Montserrat, serif',
+    mono: 'Menlo, monospace',
+  },
+});
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Container>
+    <ChakraProvider theme={appTheme}>
+      <Default>
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='*' element={<NotFound />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
-      </Container>
+      </Default>
     </ChakraProvider>
   );
 }
