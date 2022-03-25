d67c8a2f39d85c5f5405054059bdd6498d04feff
Doug Kulak <dougkulak@gmail.com>
2022-03-24 21:09:18 -0700

    fix: 404 page formatting

--- packages/frontend/src/pages/NotFound.js
+++ packages/frontend/src/pages/NotFound.js
@@ -3,26 +3,24 @@ import {Box, Center, Divider, Heading, Link, Text} from '@chakra-ui/react';
 
 function NotFound() {
   return (
-    <React.Fragment>
+    <>
       <Center>
         <Box textAlign={'center'}>
           <Box p={6}>
             <Heading>404 Not Found</Heading>
-            <Text>The page could not be found. Please check the URL and try again.</Text>
+            <Text>
+              The page could not be found. Please check the URL and try again.
+            </Text>
           </Box>
           <Divider />
           <Box p={6}>
-            <Link
-              color='teal.500'
-              href='/'
-              fontSize='2xl'
-            >
+            <Link color="teal.500" href="/" fontSize="2xl">
               Back to Home
             </Link>
           </Box>
         </Box>
       </Center>
-    </React.Fragment>
+    </>
   );
 }
 
